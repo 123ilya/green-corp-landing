0fdcdc2e17bcc3dfbbf54e8c1d385042367f82c5
ilya sobolev <123ilya@gmail.com>
2022-02-19 13:28:51 +1000

    after adding scroll event

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,5 @@
 const INCREASE_NUMBER_ANIMATION_SPEED = 50;
-
+let animationInited = false;
 function increaseNumberAnimationStep(i, element, endNumber) {
   if (i <= endNumber) {
     if (i === endNumber) {
@@ -18,7 +18,7 @@ function initIncreaseNumberAnimation() {
   let element = document.querySelector(".features__clients-count");
   increaseNumberAnimationStep(0, element, 5000);
 }
-initIncreaseNumberAnimation();
+//initIncreaseNumberAnimation(); удаляем строку
 
 /**Мой вариант счётчика
  * let counter = 0;
@@ -66,3 +66,20 @@ document
       idForm.removeChild(otherinput);
     }
   });
+//---------------------------Анимация при скроллинге-----------------------------------------------------
+function updateScroll() {
+  if (window.scrollY > 0) {
+    document.querySelector("header").classList.add("header__scrolled");
+  } else {
+    document.querySelector("header").classList.remove("header__scrolled");
+  }
+  let windowBottomPosition = window.scrollY + window.innerHeight;
+  let countElementPosition = document.querySelector(
+    ".features__clients-count"
+  ).offsetTop;
+  if (windowBottomPosition >= countElementPosition && !animationInited) {
+    animationInited = true;
+    initIncreaseNumberAnimation();
+  }
+}
+window.addEventListener("scroll", updateScroll);
